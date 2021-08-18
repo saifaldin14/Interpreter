f619cfa9ce56436c4b1416114063be3fc66dd2bb
saifaldin14 <saifaldindeveloper@gmail.com>
2021-08-18 16:14:10 -0400

    Create token.go

---
+++ token/token.go
@@ -0,0 +1,41 @@
+package token
+
+type TokenType string
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF 	= "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" //add, foobar, x, y, etc.
+	INT   = "INT"   //123456789
+
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
+
+	// Delimeters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+)
