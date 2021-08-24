42cfc6e0fcfbf748f9cfa5faf35f1d5fd48c9736
saifaldin14 <saifaldindeveloper@gmail.com>
2021-08-24 10:14:45 -0400

    Started working on the Lexer

---
+++ src/saif/lexer/lexer.go
@@ -0,0 +1,37 @@
+package lexer
+
+import (
+	"go/token"
+	"saif/token"
+)
+
+type Lexer struct {
+	input        string
+	position     int  // current position in input (points to current char)
+	readPosition int  // current reading position in input (after current char)
+	ch           byte // current char under examination
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) NewToken() token.Token {
+	var tok token.Token
+
+	l.skipWhitespace()
+
+	switch l.ch {
+	case '=':
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			literal := string(ch) + string(l.ch)
+			tok = token.Token{Type: token.EQ, Literal: literal}
+		} else {
+			tok = newToken(token.ASSIGN, l.ch)
+		}
+	}
+}
