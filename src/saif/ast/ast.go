a644106d487843e5cf3a9606acbbba11201075ab
saifaldin14 <saifaldindeveloper@gmail.com>
2021-09-05 08:22:43 -0400

    Update ast.go

--- src/saif/ast/ast.go
+++ src/saif/ast/ast.go
@@ -1,10 +1,10 @@
 package ast
 
-// import (
-// 	"bytes"
-// 	"Interpreter/src/saif/token"
-// 	"strings"
-// )
+import (
+	"Interpreter/src/saif/token"
+	"bytes"
+	// "strings"
+)
 
 //The Base Node Interface
 type Node interface {
@@ -20,4 +20,28 @@ type Statement interface {
 
 type Program struct {
 	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+func (p *Program) String() string {
+	var out bytes.Buffer
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
+// Statements
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	Name  *Identifier
+	Value Expression
 }
